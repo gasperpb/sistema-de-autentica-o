4537005d72b9a58fc09935e9e707b94bcb44370a
gasperpb <achillespb@hotmail.com>
2022-10-16 12:55:35 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import LoginPages from "./pages/LoginPages";
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPages />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/LoginPages/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginPages = () => {
+    return <h1>LoginPage</h1>
+}
+
+export default LoginPages;
